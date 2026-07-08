fa9fa866a7e1ecc96bc1749a31456757dc7510e5
M288679 <msri78848@gmail.com>
2026-07-08 14:46:42 +0530

    Update CorsConfig.java

--- src/main/java/com/example/bn1/security/CorsConfig.java
+++ src/main/java/com/example/bn1/security/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
   public CorsConfigurationSource corsConfigurationSource(){
 
     CorsConfiguration config=new CorsConfiguration();
-    config.setAllowedOrigins(List.of("http://localhost:5000", "http://localhost:5500" , "http://127.0.0.1","http://127.0.0.1:5500"));
+    config.setAllowedOrigins(List.of("http://localhost:5000", "http://localhost:5500" , "http://127.0.0.1","http://127.0.0.1:5500","https://m288679.github.io/BookNestfullstackfrontend/"));
     config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
